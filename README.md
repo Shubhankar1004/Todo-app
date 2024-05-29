3973b675e67f99cec82414d1a0082fe30b92a697
Shubhankar <shubhankarvshastri@gmail.com>
2024-05-29 23:54:54 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## ToDo Application
+
+This is a simple todo application. The user can do the following things:
+
+- Anyone can create a todo
+- Anyone can see list of todos
+- Anyone can mark the todo as done
---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { createTodo } = require("./types");
+
+const app = express()
+
+app.use(express.json())
+
+app.post("/todo", function(req, res) {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload)
+    if (!parsedPayload.success)
+    {
+        res.status(411).json({
+            msg: "You sent wrong inputs"
+        })
+    }
+})
+
+app.get("/todos", function(req, res){
+
+})
+
+app.put("/completed", function(req, res){
+    const updatePayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload)
+    if (!parsedPayload.success)
+    {
+        res.status(411).json({
+            msg: "You sent wrong inputs"
+        })
+    }
+})
